6efac4b37bba1f7b31ad17561f4dd640fa5bcb2d
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-12-16 13:21:19 -0500

    adding tests

---
+++ tests/_berryimu.cpp
@@ -0,0 +1,44 @@
+
+#include <string>
+#include <sstream>
+#include <unistd.h>
+
+#include "mary/drivers/imu/BerryIMU.h"
+
+#include "mary/tools/timeOps.h"
+#include "mary/tools/textOps.h"
+
+#include <signal.h>
+bool bRun = true;
+void CtrlC(int) {
+    bRun = false;
+}
+
+int main(int argc, char **argv) {
+    std::cout << "Connecting BerryUMI over i2c" << std::endl;
+    ImuDriver* imu = new BerryIMU();
+    imu->connect("/dev/i2c-%d", true);
+
+    bRun = true;
+    signal(SIGINT, CtrlC );
+
+    bool first_line = true;
+    const std::string deleteLine = "\e[2K\r\e[1A";
+    while (bRun) {
+        if (!first_line)
+            for (int i = 0; i < 3; i++)
+                std::cout << deleteLine;
+        first_line = false;
+
+        imu->update();
+        std::cout << " Acc: " << imu->getAcc().x << " " << imu->getAcc().y << " " << imu->getAcc().z << std::endl;
+        std::cout << " Gyr: " << imu->getGyr().x << " " << imu->getGyr().y << " " << imu->getGyr().z << std::endl;
+        std::cout << " Pitch: " << imu->getPitch() << " Roll: " << imu->getRoll() << " Heading: " << imu->getHeading() << std::endl;
+        usleep(1000);
+    }
+    imu->calibrate(false);
+    
+    imu->disconnect();
+
+    return 0;
+}
